3dc7d7b1f42f0ecbf381f553d9207341ebe8f9d1
melwinahl <melwin.ahlnas@edu.huddinge.se>
2024-04-12 09:31:27 +0200

    tidig version av inloggning form

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function clickfunktion(){
     alert("Jag är Melwin")
 }
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
